6ee36b088fa499691891b3d03c1607c2f1dc3989
srivastsh <76992595+srivastsh@users.noreply.github.com>
2023-03-13 01:05:59 -0700

    .env

--- main.py
+++ main.py
@@ -3,47 +3,50 @@ import openai
 
 openai.api_key = st.secrets["api_key"]
 
-def generate_flavor_profile(dish_input):
-    prompt = (f"Generate a flavor profile for the dish {dish_input}.")
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=100)
-    flavor_profile = response.choices[0].text.strip()
-    return flavor_profile
 
-def generate_wine_recommendations(flavor_profile, wine_preference, budget):
-    prompt = (f"Generate 2 wine recommendations for a {wine_preference} wine under {budget} dollars that pair well with a {flavor_profile} dish. For the first wine, describe why it pairs well with the dish. For the second wine, describe why it pairs well with the dish.")
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
-    wine_recommendations = response.choices[0].text.strip()
-    return wine_recommendations
+def generate_pairings(dish_input, drink_type, subcategory):
+    if drink_type == "Wine":
+        prompt = (
+            f"Generate 2 wine recommendations for a {subcategory} wine that pair well with a {dish_input} dish. For the first wine, describe why it pairs well with the dish. For the second wine, describe why it pairs well with the dish.")
+    elif drink_type == "Cocktail":
+        prompt = (
+            f"Generate a cocktail recommendation for a {subcategory} cocktail that pairs well with a {dish_input} dish.")
+    elif drink_type == "Hard Liquor":
+        prompt = (
+            f"Generate a hard liquor recommendation for a {subcategory} liquor that pairs well with a {dish_input} dish.")
+    else:
+        prompt = (
+            f"Generate a mocktail recommendation for a {subcategory} mocktail that pairs well with a {dish_input} dish.")
 
-def generate_cocktail_recommendations(flavor_profile, cocktail_preference):
-    prompt = (f"Generate 2 cocktail recipes for a {cocktail_preference} cocktail that pairs well with a {flavor_profile} dish. For the first cocktail, describe why it pairs well with the dish. For the second cocktail, describe why it pairs well with the dish.")
     response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
-    cocktail_recommendations = response.choices[0].text.strip()
-    return cocktail_recommendations
+    pairings = response.choices[0].text.strip()
+    return pairings
 
-def generate_hard_liquor_recommendations(flavor_profile, liquor_preference):
-    prompt = (f"Generate 2 {liquor_preference} liquor recommendations that pair well with a {flavor_profile} dish. For the first liquor, describe why it pairs well with the dish. For the second liquor, describe why it pairs well with the dish.")
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
-    liquor_recommendations = response.choices[0].text.strip()
-    return liquor_recommendations
-
-def generate_mocktail_recommendations(flavor_profile, mocktail_preference):
-    prompt = (f"Generate 2 mocktail recipes for a {mocktail_preference} mocktail that pairs well with a {flavor_profile} dish. For the first mocktail, describe why it pairs well with the dish. For the second mocktail, describe why it pairs well with the dish.")
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
-    mocktail_recommendations = response.choices[0].text.strip()
-    return mocktail_recommendations
 
 def main():
     st.title("Pourfect Pairings")
 
-    dish_input = st.text_input("Enter a dish or key ingredients:")
-    drink_category = st.selectbox("What kind of drink would you like?", ["Wine", "Cocktails", "Hard Liquor", "Mocktails"])
-    flavor_profile = generate_flavor_profile(dish_input)
+    drink_type = st.selectbox("What kind of drink would you like?", ["Wine", "Cocktail", "Hard Liquor", "Mocktail"])
 
-    if drink_category == "Wine":
-        wine_preference = st.selectbox("What kind of wine do you prefer?", ["Red", "White", "Rosé"])
+    if drink_type == "Wine":
+        subcategory = st.selectbox("What type of wine would you like?", ["Red", "White", "Rosé"])
         budget = st.slider("What's your budget for a bottle of wine?", 10, 100, 30)
+    elif drink_type == "Cocktail":
+        subcategory = st.selectbox("What type of cocktail would you like?",
+                                   ["Gin-based", "Vodka-based", "Rum-based", "Tequila-based", "Whiskey-based"])
+    elif drink_type == "Hard Liquor":
+        subcategory = st.selectbox("What type of hard liquor would you like?",
+                                   ["Gin", "Vodka", "Rum", "Tequila", "Whiskey"])
+    else:
+        subcategory = st.selectbox("What type of mocktail would you like?",
+                                   ["Fruity", "Citrusy", "Herbal", "Minty", "Creamy"])
 
-        if st.button("Recommend Wines"):
-            wine_recommendations = generate_wine_recommendations(flavor_profile, wine_preference, budget)
-            st.write(wine_recommendations)
+    dish_input = st.text_input("Enter a dish or key ingredients:")
+
+    if st.button("Recommend Pairings"):
+        pairings = generate_pairings(dish_input, drink_type, subcategory)
+        st.write(pairings)
+
+
+if __name__ == "__main__":
+    main()
