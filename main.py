7225ef44c3c7f3c92d2b7960de922f3f83eddbc0
srivastsh <76992595+srivastsh@users.noreply.github.com>
2023-03-16 20:54:24 -0700

    context

--- main.py
+++ main.py
@@ -61,9 +61,9 @@ def main():
             subcategory = st.selectbox("What type of mocktail would you like?",
                                        ["Any", "Fruity", "Citrusy", "Herbal", "Minty", "Creamy"])
 
-        if st.button("Recommend Pairings"):
-            pairings = generate_pairings(dish_input, drink_type, subcategory)
-            st.write(pairings)
+            if st.button("Recommend Pairings"):
+                pairings = generate_pairings(dish_input, drink_type, subcategory)
+                st.write(pairings)
 
-    if __name__ == "__main__":
-        main()
+        if __name__ == "__main__":
+            main()
